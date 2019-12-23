bacf7a7810464da58afec6dc553f57655643c4a5
Tarık Demirel <trkdmrl10@gmail.com>
2019-12-23 17:39:24 +0300

    fourth commit

---
+++ src/main/java/com/restdevice/restdevice/Configuration.java
@@ -0,0 +1,24 @@
+package com.restdevice.restdevice;
+
+import java.util.List;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
+
+@SpringBootConfiguration
+@ComponentScan(basePackages="net.kaczmarzyk")
+@EnableAutoConfiguration
+public class Configuration implements WebMvcConfigurer{
+
+	@Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationArgumentResolver());
+        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
+    }
+}
